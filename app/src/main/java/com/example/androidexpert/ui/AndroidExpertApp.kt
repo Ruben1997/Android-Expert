6345d1c7c7e15923f8356f646bed26221308ff36
Ruben Dario Rocha Lizcano <rdrocha@misena.edu.co>
2023-03-30 22:41:00 -0500

    implemented Dagger Hilt

---
+++ app/src/main/java/com/example/androidexpert/ui/AndroidExpertApp.kt
@@ -0,0 +1,9 @@
+package com.example.androidexpert.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidExpertApp : Application() {
+
+}
--- app/src/main/java/com/example/androidexpert/ui/information/InformationFragment.kt
+++ app/src/main/java/com/example/androidexpert/ui/information/InformationFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.androidexpert.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class InformationFragment : Fragment() {
+
+    private val viewModel: InformationViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
